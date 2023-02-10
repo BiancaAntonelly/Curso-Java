6006e3728084f7018945ff1f4b77397fd49e42c6
BIANCA ANTONELLY MEDEIROS DOS SANTOS <100948110+BiancaAntonelly@users.noreply.github.com>
2023-02-10 13:31:54 -0300

    atualização

---
+++ Exercício/src/Fundamentos/Console.java
@@ -0,0 +1,16 @@
+package Fundamentos;
+
+public class Console {
+	public static void main(String[] args) {
+		System.out.print("Bom");
+		System.out.print(" Dia!!!\n\n");
+		
+		System.out.println("Bom");
+		System.out.println(" Dia!!!");
+		System.out.printf("Megasena: %s %s %s %s %s %s %n", 1, 2, 3, 4, 5, 6);
+		
+		System.out.printf("Salário: %.1f%n", 6125.8297);
+		System.out.printf("Nome");
+	}
+
+}
