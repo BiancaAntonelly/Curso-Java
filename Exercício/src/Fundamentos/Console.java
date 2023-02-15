5019945914efc1a89e7376649725e601c51dfb99
BIANCA ANTONELLY MEDEIROS DOS SANTOS <100948110+BiancaAntonelly@users.noreply.github.com>
2023-02-15 08:58:59 -0300

    Console

--- Exercício/src/Fundamentos/Console.java
+++ Exercício/src/Fundamentos/Console.java
@@ -1,5 +1,7 @@
 package Fundamentos;
 
+import java.util.Scanner;
+
 public class Console {
 	public static void main(String[] args) {
 		System.out.print("Bom");
@@ -10,7 +12,21 @@ public class Console {
 		System.out.printf("Megasena: %s %s %s %s %s %s %n", 1, 2, 3, 4, 5, 6);
 		
 		System.out.printf("Salário: %.1f%n", 6125.8297);
-		System.out.printf("Nome");
+		System.out.printf("Nome: %s%n", "Bianca");
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite o seu nome:" );
+		String nome = entrada.nextLine();
+		
+		System.out.print("Digite o seu sobrenome:" );
+		String sobrenome = entrada.nextLine();
+		
+		System.out.print("Digite a sua idade: ");
+		int idade = entrada.nextInt();
+		
+		System.out.printf("%s %s tem %d anos .%n", nome, sobrenome, idade);
+		entrada.close();
+		
 	}
 
 }
